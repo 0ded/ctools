f7edf9cbdf98e63a6c519301e80ba10694631bd9
OdedGinat2 <odedginat@gmail.com>
2021-11-12 16:08:11 +0200

    -

--- bundle.py
+++ bundle.py
@@ -43,7 +43,7 @@ def unpack(flags):
         with open(OUT_FILE, "r") as source:
             files = source.readlines()[0].split(FILE_BREAK)
             for i in files:
-                print(i)
+                print(i.replace(LINE_BREAK, "\r\n"))
 
 
 def extract_flags():
